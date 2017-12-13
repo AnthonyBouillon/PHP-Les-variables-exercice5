9c4b42bd033e7fb47934291149c2e5c6dbd006ef
Anthony <anthony_bouillon@outlook.fr>
2017-12-13 08:41:43 +0100

    Les variables

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<?php
-$int = NULL;
-echo $int;
-$int = 12;
-echo $int;
-
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice5</title>
+  </head>
+  <body>
+    <?php
+    $int = NULL;
+    echo $int;
+    $int = 12;
+    echo $int;
+    ?>
+  </body>
+</html>
